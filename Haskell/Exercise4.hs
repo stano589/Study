f5a637875f4d87c60f5c482e64480cca9d4f2cf5
stanko <stano589@gmail.com>
2023-10-11 19:15:31 +0200

    Start exercise 4

---
+++ Haskell/Exercise4.hs
@@ -0,0 +1,16 @@
+-- reimplement fun1 and fun2 with incremental transformations
+
+-- product of every (even number - 2) in the list
+fun1 :: [Integer] -> Integer
+fun1 [] = 1
+fun1 (x:xs)
+    | even x = (x - 2) * fun1 xs
+    | otherwise = fun1 xs
+
+
+-- This is the Collatz conjecture 
+fun2 :: Integer -> Integer
+fun2 1 = 0
+fun2 n 
+    | even n = n + fun2 (n `div` 2)
+    | otherwise = fun2 (3 * n + 1)
